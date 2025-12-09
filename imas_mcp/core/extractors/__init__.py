81677053837f91b0abe421e989e0be749504fe22
Simon McIntosh <simon.mcintosh@iter.org>
2025-12-09 09:32:56 +0100

    refactor: Remove duplicate relationship_extractor from core/extractors
    - Delete unused relationship_extractor.py (475 lines)
    - Update __init__.py to remove conditional import
    - Clustering workflow in imas_mcp/clusters/ is now the single source

--- imas_mcp/core/extractors/__init__.py
+++ imas_mcp/core/extractors/__init__.py
@@ -8,15 +8,6 @@ from .physics_extractor import LifecycleExtractor
 from .semantic_extractor import PathExtractor, SemanticExtractor
 from .validation_extractor import ValidationExtractor
 
-# Conditionally import RelationshipExtractor to avoid heavy dependencies during build
-try:
-    from .relationship_extractor import RelationshipExtractor
-
-    _relationship_available = True
-except ImportError:
-    RelationshipExtractor = None
-    _relationship_available = False
-
 __all__ = [
     "BaseExtractor",
     "ExtractorContext",
@@ -28,6 +19,3 @@ __all__ = [
     "SemanticExtractor",
     "ValidationExtractor",
 ]
-
-if _relationship_available:
-    __all__.append("RelationshipExtractor")
